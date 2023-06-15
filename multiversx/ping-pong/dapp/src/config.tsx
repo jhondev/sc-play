b440e9d06c146288b4199b7e79bbf6ad74335dfa
jhondev <jma9974@gmail.com>
2023-06-14 22:27:02 -0500

    [mx] ping-pong

---
+++ multiversx/ping-pong/dapp/src/config.tsx
@@ -0,0 +1,15 @@
+export const contractAddress =
+  'erd19ql85h9a50z6g0pdjdl9f268qgamm9w0v82c8jpp72zxh97sq3jqskly89';
+
+export const dAppName = 'example Dapp';
+
+// Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
+export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
+
+export const apiTimeout = 6000;
+export const transactionSize = 15;
+export const TOOLS_API_URL = 'https://tools.multiversx.com';
+/**
+ * Calls to these domains will use `nativeAuth` Baerer token
+ */
+export const sampleAuthenticatedDomains = [TOOLS_API_URL];
